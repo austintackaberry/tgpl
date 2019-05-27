8dbb28907687366c913f00391f8816df3c962070
Austin Tackaberry <austintackaberry@uber.com>
2019-05-26 19:04:44 -0700

    First commit

---
+++ ch1/1_3/1_3.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	s, sep := "", ""
+	for index, arg := range os.Args[1:] {
+		fmt.Println(index)
+		fmt.Println(arg)
+		s += sep + arg
+		sep = " "
+	}
+}
